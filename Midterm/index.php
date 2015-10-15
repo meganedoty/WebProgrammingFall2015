aada25c76006bda8c227a28d29cdeef9ab211f46
meganedoty <meganedoty@gmail.com>
2015-10-15 01:08:12 +0000

    added midterm files

---
+++ Midterm/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Food Intake</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+  </head>
+  <body>
+      <div class="container">
+        <h1>Waffle Tracker</h1>
+        
+        <div class="row">
+            <div class="col-md-8 col-xs-10">
+                <a href="edit.php" class="btn btn-success">
+                    <i class="glyphicon glyphicon-thumbs-up"></i>
+                    Add waffle consumption
+                </a>
+            </div>
+        </div>
