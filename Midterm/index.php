a9c88f4ce6f4ccf273e2e171675b47f0d9f45c21
meganedoty <meganedoty@gmail.com>
2015-10-15 01:44:43 +0000

    same

--- Midterm/index.php
+++ Midterm/index.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+  $waffle = $_SESSION['waffle'];
+  if(!$waffle){
+    $_SESSION['waffle'] = $waffle = array(
+      array( 'Batter' => 'Buttermilk', 'Toppings' => 'Maple Syrup', 'Time' => strtotime("one hour ago"), Quantity => 3 ),
+      array( 'Batter' => 'Multigrain', 'Toppings' => 'Chocolate Chips', 'Time' => strtotime("now"), Quantity => 2),
+      array( 'Batter' => 'Chocolate', 'Toppings' => 'Whipped Cream', 'Time' => strtotime("now + 2 hours"), Quantity => 6),
+      array( 'Batter' => 'Blueberry', 'Toppings' => 'Ice Cream', 'Time' => strtotime("11pm"), Quantity => 2),
+      );
+  }
+  
+  $total = 0;
+  foreach ($waffle as $meal) {
+    $total += $meal['Quantity'];
+  }
+  
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -20,5 +39,36 @@
                     <i class="glyphicon glyphicon-thumbs-up"></i>
                     Add waffle consumption
                 </a>
-            </div>
-        </div>
+            </div><!-- col 8 -->
+            
+            <table class="table table-condensed table-striped table-hover">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>Batter</th>
+                  <th>Toppings</th>
+                  <th>Time</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                <?php foreach($waffle as $i => $meal): ?>
+                <tr>
+                  <th scope="row">
+                    <div class="btn-group" role="group" aria-label="...">
+                      <a href="edit.php?id=<?=$i?>" title="Edit" class="btn-xs btn-default"><i class="glyphicon glyphicon-edit"></i></a>
+                      <a href="delete.php?id=<?=$i?>" title="Delete" class="btn-xs btn-default"><i class="glyphicon glyphicon-fire"></i></a>
+                    </div>
+                  </th>
+                  <td><?=$meal['Batter']?></td>
+                  <td><?=$meal['Toppings']?></td>
+                  <td><?=date("M d Y h:i:sa", $meal['Time'])?></td>
+                  <td><?=$meal['Quantity']?></td>
+                </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+        </div> <!-- row -->
+      </div> <!-- container -->
+    </body>
+  </html>
