f4051c770d0d802937b6bb69ba3bdb681a5bec1c
meganedoty <meganedoty@gmail.com>
2015-10-26 13:39:12 +0000

    added files for php

--- Midterm/delete.php
+++ Midterm/delete.php
@@ -16,7 +16,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Food: Log Delete</title>
+    <title>Waffle: Log Delete</title>
 
     <!-- Bootstrap wtfhtmlcss dzone parse
     -->
--- Midterm/index.php
+++ Midterm/index.php
@@ -24,7 +24,7 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Food Intake</title>
+    <title>The Waffle</title>
 
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
